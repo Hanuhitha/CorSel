4a4b3770584fcf4bc62cd2b9aff6ad37f034650d
VyshnaviVollem <vyshnavivollem@gmail.com>
2023-09-22 17:30:29 -0400

    cards

--- corsel/src/components/ClassSearch.js
+++ corsel/src/components/ClassSearch.js
@@ -1,8 +1,45 @@
 import React from 'react'
 import NavBar from './NavBar'
-
+import { Button } from 'react-bootstrap'
+import {Link} from 'react-router-dom'
 export default function ClassSearch() {
   return (
-    <><NavBar></NavBar><div>ClassSearch</div></>
+    <><NavBar></NavBar>
+      <div class="container text-center">
+  <div class="row align-items-center">
+   
+    <div className="col1">  <div class="card">
+  <div class="card-body">
+    <h5 class="card-title"></h5>
+    <p class="card-text"></p>
+   
+  </div>
+</div></div>
+
+<div className="col2">  <div class="card">
+  <div class="card-body">
+    <h5 class="card-title"></h5>
+    <p class="card-text"></p>
+   
+  </div>
+</div></div>
+    <div className="col3">  <div class="card">
+  <div class="card-body">
+    <h5 class="card-title"></h5>
+    <p class="card-text"></p>
+
+  </div>
+</div> </div>
+    <div className="col4">  <div class="card">
+  <div class="card-body">
+    <h5 class="card-title"></h5>
+    <p class="card-text"></p>
+   
+  </div>
+</div> </div>
+  </div>
+</div>
+
+    </>
   )
 }
